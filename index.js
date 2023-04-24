84e19b47dfde57043b333d34e2adf5f711ae34b5
Carlos Andrés Robayo Cabra <carlosrobayocabra@gmail.com>
2023-04-24 18:38:25 -0500

    mi primer commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const mate = require("./math.js");
+console.log(mate.hola("Jos"));
+
+console.log(mate.suma(1,0));
+console.log(mate.resta(2,1));
+console.log(mate.multiplicacion(1,0));
+console.log(mate.division(1,0));
